ba1861aa3cca4e0b196ef48d79642370e273a945
Fortissimus23 <ilya33223@gmail.com>
2021-10-26 12:27:00 +0300

    update

---
+++ ООП/Лабораторные/Лаба 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+
+    enum MyEnum
+    {
+
+    }
+
+    class Train
+    {
+        string place;
+        int train_number;
+        int time;
+        int seats;
+
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Train t = new Train();
+            t.x = 4;
+            t.y = 2;
+            Console.WriteLine(t.x);
+        }
+    }
+}
